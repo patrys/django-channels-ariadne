cc7d61e1d415d27f6731ec270ba5f17cadd9207c
Patryk Zawadzki <patrys@room-303.com>
2019-01-09 12:59:13 +0100

    Move to partial, add resolver

--- mysite/routing.py
+++ mysite/routing.py
@@ -1,11 +1,31 @@
+import asyncio
+from functools import partial
+
+from ariadne import ResolverMap
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
 
-from .graphql import create_graphql_consumer
+from .graphql import GraphQLConsumer
+
+query = ResolverMap("Query")
 
 
-application = ProtocolTypeRouter({
-    "http": URLRouter([
-        url(r"^graphql/$", create_graphql_consumer("type Query { hello: String! }"))
-    ])
-})
+@query.field("hello")
+async def say_hello(root, info):
+    await asyncio.sleep(3)
+    return "Hello!"
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": URLRouter(
+            [
+                url(
+                    r"^graphql/$",
+                    partial(GraphQLConsumer, "type Query { hello: String! }", query),
+                )
+            ]
+        )
+    }
+)
+
